51a6462f3293e0adbf0357115dd601db7c109770
Fabian Altendorfer (ZM-8) <fabian.altendorfer@bmw.com>
2025-04-19 09:02:43 +0200

    bugfix prc_diagnostics

--- pm4py/algo/analysis/prc/prc_diagnostics.py
+++ pm4py/algo/analysis/prc/prc_diagnostics.py
@@ -5,7 +5,7 @@ from pm4py.objects.petri_net.obj import PetriNet, Marking
 from pm4py.algo.conformance.tokenreplay.variants import tbr_prc
 from pm4py.objects.petri_net.utils import petri_utils
 from gplearn.genetic import SymbolicRegressor
-from typing import Optional, Dict, Any, Union
+from typing import Optional, Dict, Any, Union, List
 from collections import defaultdict
 from datetime import datetime
 import os
