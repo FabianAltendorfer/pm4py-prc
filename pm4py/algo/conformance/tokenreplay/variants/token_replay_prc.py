e50d37de02e73ad0090ccc7513447e6442bae18f
FabianAltendorfer <fabian.altendorfer@hotmail.com>
2025-04-23 16:27:59 +0200

    update gespeichert

--- pm4py/algo/conformance/tokenreplay/variants/token_replay_prc.py
+++ pm4py/algo/conformance/tokenreplay/variants/token_replay_prc.py
@@ -703,7 +703,7 @@ def update_ocel_with_capacities(ocel_path: str, type_capacities: Dict[str, int],
                 capacity_elem.set('type', 'integer')
     
     tree.write(output_ocel_path)
-    print(f"Updated OCEL XML saved to '{output_ocel_path}')
+    print(f"Updated OCEL XML saved to {output_ocel_path}")
 
 def get_diagnostics_dataframe(log: Union[EventLog, pd.DataFrame], tbr_output: list[Dict[str, Any]], parameters: Optional[Dict[str, Any]] = None) -> pd.DataFrame:
     """
